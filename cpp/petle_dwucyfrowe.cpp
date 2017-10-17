984ffe3492c987a1ee123daa1dc4c413c42b8067
wiktor12v <wiktor1230@op.pl>
2017-10-17 12:05:26 +0200

    dwucyfrowe

---
+++ cpp/petle_dwucyfrowe.cpp
@@ -0,0 +1,23 @@
+/*
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namepsace std;
+int main(int argc, char **argv)
+{
+    int n=0;
+    int m=0;
+    
+   cout<<"podaj zakres <m,n> <10,99>: "<<endl;
+    
+    while(1){
+    if(m<0 || n>99 ||| m<n)
+    break;
+}
+    
+    return 0;
+}
+
--- cpp/petle_znak.cpp
+++ cpp/petle_znak.cpp
@@ -9,14 +9,20 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    char zn=;
-    while (zn !='t' || !='T' || !='n' ||!='N') 
+    char zn='t';
+    
+    while (zn=='t' || zn=='T' || zn=='n' || zn=='N') 
     {
-        cout<<"podaj miesiąc(1-12): ";
-        cin>>m;
-     }  
-   
+        cout<<"podaj znak: ";
+        cin>>zn;
+       // if (zn=='t' || zn=='T' || zn=='n' || zn=='N')
+        cout<<zn<<endl;
+        //else   
+        //break;
+     
     }
+   cout<<"brawo zgadles no i z czego sie kurwa cieszysz debilu jebany";
+    
     return 0;
 }
 
