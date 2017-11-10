03c803555f216d8e5beeb6d0d49dee3325204b6c
wiktor12v <wiktor1230@op.pl>
2017-11-10 11:44:37 +0100

    python trafione

--- python/mlotek.py
+++ python/mlotek.py
@@ -4,26 +4,46 @@
 
 from random import randint
 
-def losuj (ileliczb,maksliczb):
+
+def losuj(ileliczb, maksliczb):
     liczby = []
-    ile = 0  #  ilość wylosowanych liczb
-    while ile<ileliczb:
-     # for i in range(ileliczb):
-        liczba = randint(0,maksliczb)
+    ile = 0  # ilość wylosowanych liczb
+    while ile < ileliczb:
+        # for i in range(ileliczb):
+        liczba = randint(0, maksliczb)
         if liczby.count(liczba) == 0:
             liczby.append(liczba)
-            ile +=1
-    print(liczby)
+            ile += 1
+    return(liczby)
+
+
+def pobierz_typy(ileliczb):
+    typy = set()  # pusty zbiór
+
+    ile = 0  # ilość wylosowanych liczb
+    while ile < ileliczb:
+        # for i in range(ileliczb):
+        typ = int(input("podaj typ: "))
+        if typ not in typy:
+            typy.add(typ)
+            ile += 1
+    return typy
+
 
 def main(args):
     ileliczb = int(input("ile liczb chcesz zgadnąć?"))
     maksliczb = int(input("Podaj górny zakres"))
-    losuj (ileliczb, maksliczb)
-    typy= set()  #pusty zbiór
-    for i in range(ileliczb):
-        typ = input ("podaj typ: ")
-        typy.add(typ)
+
+    while ileliczb > maksliczb:
+        ileliczb = int(input("ile liczb chcesz zgadnąć z %s?" % maksliczb))
+
+    liczby = losuj(ileliczb, maksliczb)
+    typy = pobierz_typy(ileliczb)
+
+    print(liczby)
     print(typy)
+    trafione = set(liczby) & typy
+    print(trafione)
 
     return 0
 
