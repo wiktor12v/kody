ecd7b8ff906afc18a9a7d79f8fd2d78893e13a2f
wiktor12v <wiktor1230@op.pl>
2017-11-28 13:31:18 +0100

    ciag fibonaczieggdfgdf

---
+++ python/fibonacci.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def fib_iter(n):
+    """funckja wyswietla kolejne wyrazy ciagu fibonacciego.
+funkcja zwraca n-ty wyraz ciągu
+f(0)=0
+f(1)=1
+f(n)= f(n-2) + f(n-1) dla n > 1
+    """
+    a, b = (0, 1)
+    if n == 0:
+        print a
+        return a
+    elif n == 1:
+        print b
+        return b
+
+for i in range(1, n):
+
+
+
+def main(args):
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
