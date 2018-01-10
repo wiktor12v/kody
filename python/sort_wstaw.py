0f3b4f8b5c5d0acb7591fff1e57b989a913c18ba
wiktor12v <wiktor1230@op.pl>
2018-01-10 12:37:22 +0100

    cos tam

--- python/sort_wstaw.py
+++ python/sort_wstaw.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from math import floor
 
 def sort_wstaw(lista):
     """wersja liniowa"""
@@ -13,13 +13,25 @@ def sort_wstaw(lista):
         lista[k + 1] = el  # wstawianie elementu
     return lista
 
+def wyszukaj_bin(lewy, prawy, lista, el):
+    lewy, prawy = 0, len(lista) -1
+    while lewy < prawy:
+        srodek = floor((lewy + prawy) /2)
+        if el <= lista[srodek]:
+            prawy = srodek
+        else:
+            lewy = srodek + 1
+    return lewy #nie znaleziono elementu
+
 def sort_wstaw_bin(lista):
     for i in range(1, len(lista)):
         el = lista[i]
-        k = wyszukaj_bin_it(lewy, prawy,lista, el)
+        k = wyszukaj_bin(0, i,lista, el)
 
         #tworzenie listy z wstawionym elementem
-        lista =
+        # to do
+        lista = lista[ :k] + [el] + lista[k:i] + lista[i + 1:]
+    return lista
 
 
 def main(args):
@@ -29,6 +41,7 @@ def main(args):
     # [0, 3, 4 , 7, 2, 3, 1, 9]
     print(lista)
     print(sort_wstaw(lista))
+    sort_wstaw_bin(lista)
 
     return 0
 
