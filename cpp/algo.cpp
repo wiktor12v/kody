59ef1b2be89afe70d0b5923fa3b61ef1e8b6d804
wiktor12v <wiktor1230@op.pl>
2018-01-16 13:31:54 +0100

    lelelegmskgnfldnfl

---
+++ cpp/algo.cpp
@@ -0,0 +1,26 @@
+/*
+ * algo.cpp
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int n;
+    
+    cout<<"podaj liczbe: "<<endl;
+    cin>>n;
+     for(int i=1; i<=n; i+=2)
+     {
+         cout<<i<<endl;
+      }
+      
+         
+         
+    
+	return 0;
+}
+
