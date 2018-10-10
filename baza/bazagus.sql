a49596008c288b348534c9d4a12c57cef5af2341
wiktor12v <wiktor1230@op.pl>
2018-10-10 09:38:57 +0200

    sql

---
+++ baza/bazagus.sql
@@ -0,0 +1,43 @@
+-- BAZAGUS.SQL  
+DROP TABLE   IF EXISTS miasta;
+DROP TABLE   IF EXISTS dane_demograficzne;
+DROP TABLE   IF EXISTS powierzchnie;
+
+CREATE TABLE miasta (
+    id_miasta INTEGER PRIMARY KEY AUTOINCREMENT,
+    nazwa_miasta TEXT(30),
+    wojewodztwo TEXT(30) 
+);
+
+
+CREATE TABLE dane_demograficzne(
+
+
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+liczba_mieszkancow INTEGER,
+liczba_kobiet INTEGER,  
+grupa_wiekowa TEXT(15),
+data_akt DATE,
+id_miasta INTEGER,
+FOREIGN KEY (id_miasta) REFERENCES miasta(id_miasta)
+
+);
+
+CREATE TABLE powierzchnie (
+id INTEGER PRIMARY KEY  AUTOINCREMENT,
+powierzchnia_miasta DECIMAL,
+POWIERZCHNIA_ZIELONA DECIMAL,
+data_akt DATE,
+id_miasta INTEGER,
+FOREIGN KEY (id_miasta) REFERENCES miasta(id_miasta)
+
+);
+
+
+-- sqlite3 baza.db < bazagus.sql
+-- sqlite baza.db
+-- sqlite> .table - wyswietla tabele
+-- sqlite>.schema - wyswietla klauzule SQL
+-- sqlite<.quit - wyjscie
+
+
