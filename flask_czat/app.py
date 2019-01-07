c0000ed532eb040bba9e4f2c42e1d56e60d87051
wiktor12v <wiktor1230@op.pl>
2019-01-07 14:20:16 +0100

    trudno bylo

--- flask_czat/app.py
+++ flask_czat/app.py
@@ -3,42 +3,26 @@
 #
 #  app.py
 #  
-from flask import Flask, g
-from flask import render_template
+from flask import g
 from modele import *
+from views import *
+import os
 
-app = Flask(__name__)
 app.config.update(dict(
-    SEKRETNY_KLUCZ='bardzotajnyklucz',
-    TYTUL='Czat'
+    SECRET_KEY='bardzotajnyklucz',
+    TITLE='Czat'
 ))
-
+# DATABASE=os.path.join(app.root_path, baza_nazwa)
 @app.before_request
 def before_request():
     g.db = baza
-    g.db.connect()
+    g.db.connect(reuse_if_open=True)
 
 @app.after_request
 def after_request(response):
     g.db.close()
     return response
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/quiz')
-def quiz():
-    pytania = Pytanie.select().join(Odpowiedz).distinct().order_by(Pytanie.id)
-    return render_template('quiz.html', query = pytania)
-
-
-@app.route('/klasa')
-def klasa():
-    return render_template('klasa.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
-    
     
