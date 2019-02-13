a9c7674aefddcd43aacda35a5bd71d91dde134fb
wiktor12v <wiktor1230@op.pl>
2019-02-13 09:41:26 +0100

    stuktura

--- cpp zaawansowane/wektor.cpp
+++ cpp zaawansowane/wektor.cpp
@@ -11,14 +11,54 @@
 //wektor - do przechowywania wspólrzednych punktu początkowego i koncowego wektora
 #include <iostream>
 #include <fstream>
-
+ 
 using namespace std;
-
+ 
 struct punkt
-
+{
+    int x;
+    int y;
+};
+ 
+struct wektor
+{
+    punkt pp;
+    punkt pk;
+   
+};
+ 
+punkt wylicz_srodek(wektor w) {
+    
+    punkt srodek;
+     srodek.x = (w.pp.x+w.pk.x)/2;      
+     srodek.y = (w.pp.y+w.pk.y)/2;      
+    return srodek;
+   
+}
+ 
+ 
+ 
+wektor getWektor() {
+    wektor w1;
+    cout << "Podaj x, y punktu poczatkowego: ";
+    cin >> w1.pp.x;
+    cin >> w1.pp.y;
+    cout << "Podaj x, y punktu końcowego: ";
+    cin >> w1.pk.x;
+    cin >> w1.pk.y;
+ 
+   
+    return w1;
+   
+}
+ 
+ 
+ 
 int main(int argc, char **argv)
 {
-	
-	return 0;
+    wektor w;
+    punkt srodek;
+    w = getWektor();
+    
+        return 0;
 }
-
